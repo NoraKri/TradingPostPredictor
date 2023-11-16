9f47160b0bc113c08f200eb2fbf3d501696838f3
Nora Kristiansen <106915799+NoraKri@users.noreply.github.com>
2023-11-16 23:10:28 +0100

    add some methods for data fetching

--- api/datafetching.py
+++ api/datafetching.py
@@ -1,10 +1,24 @@
 ﻿import gw2api
 import json
 
+
+# Fetches all item ids in the game
 def get_items_ids():
     url = gw2api.request_items()
-    data = gw2api.fetch_requests([url],1,60)
+    data = gw2api.fetch_requests([url], 1, 60)
     allItems = json.loads(data[0])
     return allItems
 
-print(get_items_ids())
+# Fetches detailed info on each item
+def get_items_info(items):
+    url = gw2api.request_item_details(items)
+    data = gw2api.fetch_requests([url], 5, 60)
+    itemInfo = json.loads(data[0])
+    return itemInfo
+
+# Fetches item prices for each item
+def get_item_prices(items):
+    url = gw2api.request_prices(items)
+    data = gw2api.fetch_requests([url], 5, 60)
+    itemPrices = json.loads(data[0])
+    return itemPrices
